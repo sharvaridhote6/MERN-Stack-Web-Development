b18be2e8b2cb5aecd421be28bb0a938138464673
Sharvari Dhote <dhotesharvari6@gmail.com>
2024-11-01 16:16:54 +0530

    new commits

---
+++ Jsfoundation/basics.js
@@ -0,0 +1,6 @@
+
+console.log("Hello");
+process.stdout.write("hi"); //different and longer way which doesn't give the output ona different line
+// process.stdout.write("hi"); 
+console.table({name:"sharvari"});
+console.table({city:"pune"});
---
+++ Jsfoundation/datatypes.js
@@ -0,0 +1,17 @@
+// var score=102 older version
+let score=102
+let name= "Sharvari" //string
+let isLoggedin = true //boolean
+
+//object
+let teaTypes=["lemontea","green tea"] //array
+let user= {name: "sharu", city:"pune"} 
+ 
+let getScore= score; //borrowing score value to getscore
+
+console.log(getScore); //102
+
+let gameName="spiderman";
+gameName="spideyy";
+console.log(gameName);
+
