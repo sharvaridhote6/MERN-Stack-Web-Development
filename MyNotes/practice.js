776a62fb6e3e603752a2fa88b3dfd8cbb95972ec
Sharvari Dhote <dhotesharvari6@gmail.com>
2025-01-01 15:01:04 +0530

    deletions and errors resolved

--- MyNotes/practice.js
+++ MyNotes/practice.js
@@ -54,4 +54,6 @@ console.log(student1);
 
 // Create a constructor function Animal that has a method speak() that return 'Animal speaking'.
 // Then create another constructor Dog that inherits from Animal using prototypes.
-// The Dog constructor should add a method bark() that returns 'Woof!'. Demonstrate the prototype chain between Dog and Animal.
+// The Dog constructor should add a method bark() that returns 'Woof!'. Demonstrate the prototype chain between Dog and Animal. 
+
+
