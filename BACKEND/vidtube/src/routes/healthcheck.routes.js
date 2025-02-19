d7b76b39649d956e3eb968ee2bdb58b657c43e6e
Sharvari Dhote <dhotesharvari6@gmail.com>
2025-02-19 21:14:05 +0530

    commit changes

--- BACKEND/vidtube/src/routes/healthcheck.routes.js
+++ BACKEND/vidtube/src/routes/healthcheck.routes.js
@@ -2,6 +2,6 @@ import {Router} from 'express';
 import {healthCheck} from '../controllers/healthcheck.controller.js';
 
 const router=Router();
-router.route('/').get(healthCheck);//this is used to check the health of the application 
+router.route('/').get(healthCheck);//calls healthcheck to access it  
 
 export default router;
